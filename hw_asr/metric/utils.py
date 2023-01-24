2f6f1507bad4f0862125f21788ed1e8749caafb2
Aleksandr Strygin <strigin-sasha2015@yandex.ru>
2023-01-24 18:40:11 +0300

    added implementation of calc_cer and calc_wer

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,10 +1,16 @@
+import editdistance
+
+
 # Don't forget to support cases when target_text == ''
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if target_text == '':
+        return len(predicted_text)
+    return editdistance.distance(target_text, predicted_text) / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if target_text == '':
+        return len(predicted_text.split(' '))
+    splitted_target = target_text.split(' ')
+    return editdistance.distance(splitted_target, predicted_text.split(' ')) / len(splitted_target)
